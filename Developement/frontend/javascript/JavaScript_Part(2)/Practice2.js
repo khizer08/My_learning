0ffbb3231fb0a436eba560aaa58880c151f0283e
khizer08 <syedkhizer2004@gmail.com>
2025-09-20 11:48:02 +0530

    learning

--- Developement/frontend/javascript/JavaScript_Part(2)/Practice2.js
+++ Developement/frontend/javascript/JavaScript_Part(2)/Practice2.js
@@ -1,14 +1,15 @@
-let size=prompt("enter the size: ");
-if(size=="XL"){
+let size=prompt("enter the size(xl,l,m,s): ");
+size=size.toLowerCase();
+if(size=="xl"){
     console.log("The price is 250");
 }
-else if(size=="L"){
+else if(size=="l"){
     console.log("The price is 200");
 }
-else if(size=="M"){
+else if(size=="m"){
     console.log("The price is 100");
 }
-else if(size=="S"){
+else if(size=="s"){
     console.log("The price is 50");
 }
 else{
