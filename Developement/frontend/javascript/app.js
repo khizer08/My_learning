2e1d748fa54114cb0b4c14f4d7ef98d6decf8ce5
khizer08 <syedkhizer2004@gmail.com>
2025-09-22 22:35:12 +0530

    22/09

--- Developement/frontend/javascript/app.js
+++ Developement/frontend/javascript/app.js
@@ -1,6 +1,9 @@
-let input=document.querySelector("input");
-
-input.addEventListener("input",function(){
-    let h1=document.querySelector("h1");
-    h1.innerText=input.value;
-})
+let arr1=[1,2,3];
+let arr2=arr1;
+console.log(arr1);
+console.log(arr2);
+if(arr1==arr2){
+    console.log("both are same");
+}else{
+    console.log("both are different");
+}
