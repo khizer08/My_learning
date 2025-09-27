8601f7f9c5e0a906994a92a54b45d447732ded1b
khizer08 <syedkhizer2004@gmail.com>
2025-09-27 10:14:11 +0530

    27/09

--- Developement/frontend/javascript/JavaScript_Part(3)/Practice3.js
+++ Developement/frontend/javascript/JavaScript_Part(3)/Practice3.js
@@ -5,8 +5,10 @@ console.log(arr1);
 
 //second
 let arr2=["c","c++","html","javascript","python","java","c#","sql"];
+console.log(arr2);
 index=arr2.indexOf("javascript");
 console.log(`the index of string "javascript" before reverse is ${index}`);
 arr2.reverse();
+console.log(arr2);
 index=arr2.indexOf("javascript");
 console.log(`the index of string "javascript" after reverse is ${index}`);
