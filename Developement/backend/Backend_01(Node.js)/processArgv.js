0648c36a9b83d407f69f6111d822626d90174be1
khizer08 <syedkhizer2004@gmail.com>
2025-09-27 16:12:04 +0530

    27/09

--- Developement/backend/Backend_01(Node.js)/processArgv.js
+++ Developement/backend/Backend_01(Node.js)/processArgv.js
@@ -1,6 +1,6 @@
 let args=process.argv;
 console.log (args);
-for (let i=2;i<args.length;i++){
+for (let i=2;i<args.length;i++){ // why i=2? ,because first two index has default argument and current working directory 
     console.log("hello",args[i]);
 }
 
