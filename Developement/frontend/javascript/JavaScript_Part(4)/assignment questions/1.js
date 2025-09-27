fcdd410a393ea77ffc725383c75cba89d9c813d3
khizer08 <syedkhizer2004@gmail.com>
2025-09-27 12:15:10 +0530

    27/09

--- Developement/frontend/javascript/JavaScript_Part(4)/assignment questions/1.js
+++ Developement/frontend/javascript/JavaScript_Part(4)/assignment questions/1.js
@@ -1,4 +1,5 @@
 let arr1=[1,2,3,4,5,67,22,2,1,4,3,2,4,2];
+console.log(arr1.length);
 let num=prompt("Enter the number you want to delete");
 for(let i=0;i<arr1.length;i++){
     if(arr1[i]==num){
@@ -6,3 +7,4 @@ for(let i=0;i<arr1.length;i++){
     }
 }
 console.log(arr1);
+// this program will delete all occurance of that number "num"
