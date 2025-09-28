d73cf24663752965b30405178b6cbd83b5c04d41
khizer08 <syedkhizer2004@gmail.com>
2025-09-28 18:20:14 +0530

    28/09

---
+++ Developement/backend/Backend_03(Node_EJS)/index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const path=require("path");
+const app=express();
+
+const port=8080;
+
+app.set("views",path.join(__dirname,"/views"));
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+})
+
+
+app.listen(port,()=>{
+    console.log("server is running on port 3000");
+});
