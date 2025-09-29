fead337d52a03935726317f592eeb4e70811272f
khizer08 <syedkhizer2004@gmail.com>
2025-09-29 19:12:16 +0530

    29/09

--- Developement/backend/Backend_03(Node_EJS)/activity/index.js
+++ Developement/backend/Backend_03(Node_EJS)/activity/index.js
@@ -6,6 +6,8 @@ const port=3000;
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"public/css")));
+app.use(express.static(path.join(__dirname,"public/js")));
 
 app.get("/ig/:username",(req,res)=>{
     let {username}=req.params;
---
+++ Developement/backend/Backend_03(Node_EJS)/activity/public/js/app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelectorAll("button");
+
+for (btns of btn){
+    addEventListener("click",()=>{
+        console.log("button was clicked");
+    })
+}
