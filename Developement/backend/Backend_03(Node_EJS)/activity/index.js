d7712a998d22c43b52d5c98d3bd4d9d06d12e3b4
khizer08 <syedkhizer2004@gmail.com>
2025-09-29 18:24:33 +0530

    29/09

---
+++ Developement/backend/Backend_03(Node_EJS)/activity/index.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const path=require("path");
+const app=express();
+
+const port=3000;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.get("/ig/:username",(req,res)=>{
+    let {username}=req.params;
+    const instaData=require("./data.json");// json file acts as database here.
+    let data=instaData[username];//fetching only which page we want.
+    if(data){
+        res.render("instagram.ejs",{data});
+    }else{
+        res.render("error.ejs");
+    }
+
+});
+
+app.listen(port,()=>{
+    console.log(`the server is running at port ${port}`);
+});
