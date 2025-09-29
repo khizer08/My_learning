b605e325cbaf650e8bb5ee847534b3cd63e2b8a3
khizer08 <syedkhizer2004@gmail.com>
2025-09-29 14:55:09 +0530

    28/09

--- Developement/backend/Backend_03(Node_EJS)/index.js
+++ Developement/backend/Backend_03(Node_EJS)/index.js
@@ -1,15 +1,26 @@
 const express=require('express');
-const path=require("path");
+const path=require("path"); // for setting path we required "path" package. 
 const app=express();
 
 const port=8080;
 
-app.set("views",path.join(__dirname,"/views"));
+app.set("views",path.join(__dirname,"/views")); // telling where the "views" folder/directory will be present.
 app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
     res.render("home.ejs");
-})
+});
+
+app.get("/rolldice",(req,res)=>{
+    const diceVal=Math.floor(Math.random()*6)+1;
+    res.render("rollDice.ejs",{diceVal});
+});
+
+app.get("/ig/:username",(req,res)=>{
+    const {username}=req.params;
+    const followers=["huehue","dogesh","meow"];
+    res.render("instagram.ejs",{username,followers});
+});
 
 
 app.listen(port,()=>{
