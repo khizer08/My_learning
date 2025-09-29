882e3ddf89976ae1fe89adae86500d91326b5e9c
khizer08 <syedkhizer2004@gmail.com>
2025-09-29 19:58:52 +0530

    29/09

--- Developement/backend/Backend_03(Node_EJS)/activity/index.js
+++ Developement/backend/Backend_03(Node_EJS)/activity/index.js
@@ -5,7 +5,7 @@ const app=express();
 const port=3000;
 
 app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
+app.set("views",path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public/css")));
 app.use(express.static(path.join(__dirname,"public/js")));
 
