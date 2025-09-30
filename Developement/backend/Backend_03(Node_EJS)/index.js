83c11bcf6f989c806c15d649b68fb7a48af5a9f6
khizer08 <syedkhizer2004@gmail.com>
2025-09-30 18:06:08 +0530

    30/09

--- Developement/backend/Backend_03(Node_EJS)/index.js
+++ Developement/backend/Backend_03(Node_EJS)/index.js
@@ -4,16 +4,11 @@ const app=express();
 
 const port=8080;
 
-app.set("views",path.join(__dirname,"/views")); // telling where the "views" folder/directory will be present.
+app.set("views",path.join(__dirname,"views")); // telling where the "views" folder/directory will be present.
 app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
     res.render("home.ejs");
-});
-
-app.get("/rolldice",(req,res)=>{
-    const diceVal=Math.floor(Math.random()*6)+1;
-    res.render("rollDice.ejs",{diceVal});
 });
 
 app.get("/ig/:username",(req,res)=>{
@@ -22,7 +17,13 @@ app.get("/ig/:username",(req,res)=>{
     res.render("instagram.ejs",{username,followers});
 });
 
+app.get("/rolldice",(req,res)=>{
+    const diceVal=Math.floor(Math.random()*6)+1;
+    res.render("rollDice.ejs",{diceVal});
+});
+
+
 
 app.listen(port,()=>{
-    console.log("server is running on port 3000!");
+    console.log("server is running on port 8080!");
 });
