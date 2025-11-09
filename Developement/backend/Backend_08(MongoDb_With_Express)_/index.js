ca416599ac642e1df8c2d62d5892c9d08aed264a
khizer08 <syedkhizer2004@gmail.com>
2025-11-09 13:33:26 +0530

    09/11

---
+++ Developement/backend/Backend_08(MongoDb_With_Express)_/index.js
@@ -0,0 +1,68 @@
+const express = require ("express");
+const mongoose = require('mongoose');
+const path=require("path");
+const Chat=require("./models/chat.js");
+
+const app=express();
+const port=8080;
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")))
+app.use(express.urlencoded({extended:true}));
+
+main()
+.then(()=>{
+    console.log("connection successful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+
+// home route
+app.get("/",(req,res)=>{
+    res.render("home.ejs");
+});
+
+
+// all chats route
+app.get("/chats",async (req,res)=>{
+    let chats=await Chat.find();
+    console.log(chats);
+    res.render("index.ejs",{chats});
+});
+
+
+// newchat route
+app.get("/chats/new",(req,res)=>{
+    res.render("new.ejs")
+});
+
+
+// newchat (db) route
+app.post("/chats",(req,res)=>{
+    let {from,to,msg}=req.body;
+    let newChat=new Chat({
+        from:from,
+        to:to,
+        msg:msg,
+        created_at:new Date()
+    });
+
+    newChat.save()
+    .then((res)=>{
+        console.log("chat was saved")
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+    res.redirect("/chats");
+});
+
+
+app.listen(port,()=>{
+    console.log("port listening to",port);
+});
---
+++ Developement/backend/Backend_08(MongoDb_With_Express)_/init.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const Chat=require("./models/chat.js");
+
+
+main()
+.then(()=>{
+    console.log("connection successful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+
+let allChats=[
+    {
+    from:"neha",
+    to:"rohan",
+    msg:"send me your exam sheets",
+    created_at:new Date(),
+   },
+    {
+    from:"syed",
+    to:"khizer",
+    msg:"huehue",
+    created_at:new Date(),
+   },
+    {
+    from:"mohan",
+    to:"rohit",
+    msg:"holla",
+    created_at:new Date(),
+   },
+    {
+    from:"python",
+    to:"java",
+    msg:"hello world",
+    created_at:new Date(),
+   },
+   
+];
+
+Chat.insertMany(allChats);
