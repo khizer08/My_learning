106a536eff16f8784131687e4286d49f5765378d
khizer08 <syedkhizer2004@gmail.com>
2025-11-09 13:35:38 +0530

    09/11

--- Developement/backend/Backend_08(MongoDb_With_Express)_/index.js
+++ Developement/backend/Backend_08(MongoDb_With_Express)_/index.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const path=require("path");
 const Chat=require("./models/chat.js");
 
-const app=express();
 const port=8080;
+const app=express();
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
