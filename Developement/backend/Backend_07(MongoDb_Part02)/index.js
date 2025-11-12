d8bbec40174624f02e8a465a50bb78aca49319c1
khizer08 <syedkhizer2004@gmail.com>
2025-11-12 18:46:13 +0530

    12/11

--- Developement/backend/Backend_07(MongoDb_Part02)/index.js
+++ Developement/backend/Backend_07(MongoDb_Part02)/index.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
 
-main()// function which returns "promise" we can make use of "then to get result" and "catch to get error".
+
+main() //function which returns "promise" we can make use of "then to get result" and "catch to get error".
 .then((res)=>{
     console.log("connection successful");
 })
 .catch(err => console.log(err));
 
-async function main() { // as "main() is a async function " it returns "promise" .
+async function main() { //as "main() is a async function " it returns "promise" .
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 } 
-// the above lines are from "mongoosejs.com".
+//the above lines are from "mongoosejs.com".
 
-const userSchema=new mongoose.Schema({ // this is called "Schema".
+const userSchema=new mongoose.Schema({ //this is called "Schema".
     name:String,
     email:String,
     age:Number,
@@ -20,8 +21,7 @@ const userSchema=new mongoose.Schema({ // this is called "Schema".
 const User=mongoose.model("User",userSchema); // "model" creation.
 
 
-
-// User.insertMany([ // inserting many documents at a time.
+// User.insertMany([ //inserting many documents at a time.
 //   {name:"tony",email:"tony@gmail.com",age:50},
 //   {name:"peter",email:"peter@gmail.com",age:40},
 //   {name:"bruce",email:"bruce@gmail.com",age:30},
@@ -29,11 +29,13 @@ const User=mongoose.model("User",userSchema); // "model" creation.
   //   console.log(res);
 // });
 
+
 // const user2= new User({ // inserting single document at a time (save b krna hai).
 //   name:"eve",
 //   email:"eve@yahoo.com",
 //   age:48,
 // });
+
 
 // user2
 // .save() // "save()" is a async function which returns promise.
@@ -60,6 +62,7 @@ const User=mongoose.model("User",userSchema); // "model" creation.
 //   console.log(err);
 // });
 
+
 // User.findOneAndUpdate({name:"bruce"},{age:60},{new:true})
 // .then((res)=>{
 //   console.log(res);
@@ -85,12 +88,14 @@ const User=mongoose.model("User",userSchema); // "model" creation.
 //   console.log(err);
 // });
 
+
 // User.deleteMany({age:{$gt:49}}).then((res)=>{
 //   console.log(res);
 // })
 // .catch((err)=>{
 //   console.log(err);
 // });
+
 
 // User.findByIdAndDelete("690f029d03ddc01f6ba5aebe")
 // .then((res)=>{
@@ -99,6 +104,7 @@ const User=mongoose.model("User",userSchema); // "model" creation.
 // .catch((err)=>{
 //   console.log(err);
 // });
+
 
 User.findOneAndDelete({name:"eve"})
 .then((res)=>{
