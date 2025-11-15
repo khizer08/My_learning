660dee4f17998eef9dbfdf6df6c7131c8a396476
khizer08 <syedkhizer2004@gmail.com>
2025-11-15 11:31:35 +0530

    15/11

--- Developement/backend/Backend_08(MongoDb_With_Express)/index.js
+++ Developement/backend/Backend_08(MongoDb_With_Express)/index.js
@@ -3,6 +3,7 @@ const path=require("path");
 const methodOverride=require("method-override");
 const mongoose = require('mongoose');
 const Chat=require("./models/chat.js");// this folder is created by us.
+const ExpressError=require("./ExpressError.js");
 
 const port=8080;
 const app=express();
@@ -22,7 +23,7 @@ main()
 });
 
 async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+    await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 }
 
 
@@ -33,14 +34,15 @@ app.get("/",(req,res)=>{
 
 
 // all chats route
-app.get("/chats",async (req,res)=>{
+app.get("/chats",asyncWrap(async (req,res)=>{
     let chats=await Chat.find();
     res.render("allChats.ejs",{chats});
-});
+}));
 
 
 // newchat route
 app.get("/chats/new",(req,res)=>{
+    // throw new ExpressError(401,"unathorized"); // as it is not "async" we can handle the error easily
     res.render("new.ejs")
 });
 
@@ -66,16 +68,35 @@ app.post("/chats",(req,res)=>{
 });
 
 
+
+function asyncWrap(fn){ // function to handle errors for "async functions" (better way) 
+    return function(req,res,next){
+        fn(req,res,next).catch((err)=>next(err));
+    };
+}
+
+//NEW -SHOW ROUTE
+app.get("/chats/:id",asyncWrap(async (req,res,next)=>{
+    let {id}=req.params;
+    let chat=await Chat.findById(id);
+    if(!chat){
+        next(new ExpressError(401,"chat not found"));
+        // throw new ExpressError(500,"chat not found"); // from "express 5 version onewards this will also work".
+    }
+    res.render("edit.ejs",{chat});
+}));
+
+
 // edit route
-app.get("/chats/:id/edit",async(req,res)=>{
+app.get("/chats/:id/edit",asyncWrap(async(req,res)=>{
     let {id}=req.params;
     let chat=await Chat.findById(id);
     res.render("edit.ejs",{chat});
-});
+}));
 
 
 //update route
-app.put("/chats/:id",async (req,res)=>{
+app.put("/chats/:id",asyncWrap(async (req,res)=>{
     let {id}=req.params;
     let {msg:newMsg}=req.body; // do remember how data is coming from body.
     let updatedChat=await Chat.findByIdAndUpdate(
@@ -85,17 +106,23 @@ app.put("/chats/:id",async (req,res)=>{
     );
     console.log(updatedChat);
     res.redirect("/chats");
-});
+}));
 
 
 //destroy route
-app.delete("/chats/:id",async (req,res)=>{
+app.delete("/chats/:id",asyncWrap(async (req,res)=>{
     let{id}=req.params;
     let deletedChat=await Chat.findByIdAndDelete(id);
     console.log(deletedChat);
     res.redirect("/chats");
-});
+}));
 
+
+//Error handling Middleware
+app.use((err,req,res,next)=>{
+    let{status=500,message="some error occured"}=err;
+    res.status(status).send(message);
+});
 
 app.listen(port,()=>{
     console.log("port listening to",port);
