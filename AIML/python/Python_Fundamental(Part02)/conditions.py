c05b16cbedba44fd04e9218a54d184bc325c3f9f
khizer08 <syedkhizer2004@gmail.com>
2025-11-16 20:52:59 +0530

    16/11

---
+++ AIML/python/Python_Fundamental(Part02)/conditions.py
@@ -0,0 +1,17 @@
+# "if" and "else" 
+age=20
+if(age>21):
+    print("you can vote")
+else:
+    print("sorry you cannot vote")
+
+# "elif"
+color=input("enter the trafic light color: ")
+if (color=="red"):
+    print("stop")
+elif(color=="green"):
+    print("go")
+elif(color=="yellow"):
+    print("get ready")
+else:
+    print("this isnt a trafic light color!!")
---
+++ AIML/python/Python_Fundamental(Part02)/odd_even.py
@@ -0,0 +1,6 @@
+number=int(input("enter number to check if it is even or odd: "))
+
+if(number%2==0):
+    print("it is even")
+else:
+    print("it is odd")
