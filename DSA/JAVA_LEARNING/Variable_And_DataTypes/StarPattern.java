cb67d57db5cc71072ab57f0dee2a481eb48a8d57
khizer08 <syedkhizer2004@gmail.com>
2026-01-03 09:06:56 +0530

    03/01

--- DSA/JAVA_LEARNING/Variable_And_DataTypes/StarPattern.java
+++ DSA/JAVA_LEARNING/Variable_And_DataTypes/StarPattern.java
@@ -1,8 +1,12 @@
 public class StarPattern{
     public static void main(String args[]){
-        System.out.println("****");
-        System.out.println("***");
-        System.out.println("**");
-        System.out.println("*");
+        for(int i=4;i>=1;i--){
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
     }
 }
