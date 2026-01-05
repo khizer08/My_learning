ceef20d487b1592a6ad179a77affd88de233f75c
khizer08 <syedkhizer2004@gmail.com>
2026-01-05 21:06:17 +0530

    loops

--- DSA/JAVA_LEARNING/Loops/While_sum_n_no.java
+++ DSA/JAVA_LEARNING/Loops/While_sum_n_no.java
@@ -2,5 +2,14 @@ import java.util.*;
 public class While_sum_n_no{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n=sc.nextInt();
+        int count=1;
+        int sum=0;
+        while(count<=n){
+            sum+=count;
+            count++;
+        }
+        System.out.println("sum of 1 to "+n+" is: "+sum);
     }
 }
