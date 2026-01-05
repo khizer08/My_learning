4c3ba29b750f74a71a33627c97a969fb44d056b0
khizer08 <syedkhizer2004@gmail.com>
2026-01-05 22:34:40 +0530

    loops advanced

--- DSA/JAVA_LEARNING/Loops/for_reverse_no.java
+++ DSA/JAVA_LEARNING/Loops/for_reverse_no.java
@@ -1,16 +1,13 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class for_reverse_no {
-
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the number: ");
-        int n = sc.nextInt();
-
-        for (int i = n; i >= 1; i--) {
-            System.out.println(i);
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of number: ");
+        int number=sc.nextInt();
+        while(number>0){
+            int lastDigit=number%10;
+            System.out.print(lastDigit);
+            number=number/10;
         }
         sc.close();
     }
