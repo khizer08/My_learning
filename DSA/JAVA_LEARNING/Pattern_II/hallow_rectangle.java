90e78954d1f4ea556afdf075f0d8e794849797b2
khizer08 <syedkhizer2004@gmail.com>
2026-01-07 20:41:12 +0530

    hallow-rectangle

---
+++ DSA/JAVA_LEARNING/Pattern_II/hallow_rectangle.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class hallow_rectangle {
+    public static void main(String[] args) {
+        int number,i,j;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the value of number: ");
+        number=sc.nextInt();
+        for(i=1;i<=number;i++){
+            for(j=1;j<=number+1;j++){
+                if(i==1 || i==number || j==1 || j==number+1){
+                    System.out.print("*");
+                }else{
+                System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
