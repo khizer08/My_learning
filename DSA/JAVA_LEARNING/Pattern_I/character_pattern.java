2e991ddd90eee40e0db570dc9bd884a60e56ccfa
khizer08 <syedkhizer2004@gmail.com>
2026-01-07 19:53:56 +0530

    character-pattern

---
+++ DSA/JAVA_LEARNING/Pattern_I/character_pattern.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class character_pattern{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i,j,number;
+        char ch='A';
+        System.out.print("Enter the value number: ");
+        number=sc.nextInt();
+        for(i=1;i<=number;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
