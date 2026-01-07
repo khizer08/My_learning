8c05395490455d295e3a20b8223931e86d4159cd
khizer08 <syedkhizer2004@gmail.com>
2026-01-07 19:42:24 +0530

    half-pyramid

---
+++ DSA/JAVA_LEARNING/Pattern_I/half_pattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class half_pattern{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i,j,number;
+        System.out.print("Enter the value of number: ");
+        number=sc.nextInt();
+        for(i=1;i<=number;i++){
+            for(j=1;j<=i;j++){
+            System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
