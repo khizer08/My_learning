9d68531327eb3a9c1bbef0a71c863979e1b6ea3b
khizer08 <syedkhizer2004@gmail.com>
2026-01-07 22:19:15 +0530

    functions-brush-up

---
+++ DSA/JAVA_LEARNING/functions_methods/task.java
@@ -0,0 +1,14 @@
+// import java.util.*;
+public class task{
+    public static int hello(){
+        System.out.println("hello world");
+        return 0;
+    }
+    public static void main(String[] args) {
+        hello();
+        hello();
+        hello();
+        hello();
+        hello();
+    }
+}
