c5f92a26c6253f6539e53ac8a51ce545637b58e7
khizer08 <syedkhizer2004@gmail.com>
2026-01-07 19:30:56 +0530

    reverse-star

---
+++ DSA/JAVA_LEARNING/Pattern_I/reverse_star.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class reverse_star{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i,j,number;
+        System.out.print("Enter the value of number: ");
+        number=sc.nextInt();
+        for(i=number;i>0;i--){
+            for(j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
