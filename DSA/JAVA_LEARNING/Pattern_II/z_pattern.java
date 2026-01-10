7fc4aa9c608b39190889157db5f884a3422e1c5a
khizer08 <syedkhizer2004@gmail.com>
2026-01-10 21:02:33 +0530

    z-pattern

---
+++ DSA/JAVA_LEARNING/Pattern_II/z_pattern.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class z_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        for(int i=1;i<n-1;i++) {
+            for(int j=1;j<n-i;j++) {
+                System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+
+        for(int i=1;i<=n;i++) {
+            System.out.print("*");
+        }
+        sc.close();
+    }
+}
