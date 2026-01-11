435a2252e982baa9c5d3a83a1c33daed63bc7407
khizer08 <syedkhizer2004@gmail.com>
2026-01-11 12:31:35 +0530

    readibility

--- DSA/JAVA_LEARNING/Pattern_II/z_pattern.java
+++ DSA/JAVA_LEARNING/Pattern_II/z_pattern.java
@@ -6,19 +6,19 @@ public class z_pattern {
         int n = sc.nextInt();
 
         for(int i=1;i<=n;i++) {
-            System.out.print("*");
+            System.out.print("*");// first line logic
         }
         System.out.println();
 
         for(int i=1;i<n-1;i++) {
             for(int j=1;j<n-i;j++) {
-                System.out.print(" ");
+                System.out.print(" ");// center logic 
             }
             System.out.println("*");
         }
 
         for(int i=1;i<=n;i++) {
-            System.out.print("*");
+            System.out.print("*");// last line logic
         }
         sc.close();
     }
