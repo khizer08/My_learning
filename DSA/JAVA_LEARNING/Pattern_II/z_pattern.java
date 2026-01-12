d683ae4cf204d124f5899df8fb23ddd0be12fee7
khizer08 <syedkhizer2004@gmail.com>
2026-01-12 15:07:47 +0530

    ai-solved

--- DSA/JAVA_LEARNING/Pattern_II/z_pattern.java
+++ DSA/JAVA_LEARNING/Pattern_II/z_pattern.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class z_pattern {
+public class z_pattern {// try again
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
