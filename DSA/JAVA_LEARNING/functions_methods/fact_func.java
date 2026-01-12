704414506fb5396e3d213a528bddeaa89860c80b
khizer08 <syedkhizer2004@gmail.com>
2026-01-12 22:31:17 +0530

    fact_func

---
+++ DSA/JAVA_LEARNING/functions_methods/fact_func.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class fact_func{
+    public static int fact(int n){
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int result=fact(10);
+        System.out.println(result);
+        sc.close();
+    }
+}
