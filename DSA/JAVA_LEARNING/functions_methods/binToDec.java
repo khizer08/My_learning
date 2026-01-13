1980c3f7201d9d2b6a3983a9c008d354c3be3568
khizer08 <syedkhizer2004@gmail.com>
2026-01-13 14:22:42 +0530

    binary-to-decimal

--- DSA/JAVA_LEARNING/functions_methods/binToDec.java
+++ DSA/JAVA_LEARNING/functions_methods/binToDec.java
@@ -10,6 +10,6 @@ public class binToDec{
         return decimal;
     }
     public static void main(String[] args) {
-        System.out.println(binToDecFun(1000));
+        System.out.println(binToDecFun(10111));
     }
 }
