5cb6c9e55d046d41d5184dfdf24ec34f47cc9228
khizer08 <syedkhizer2004@gmail.com>
2026-01-14 22:35:10 +0530

    sum-of-digits

--- DSA/JAVA_LEARNING/functions_methods/Practice_Questions/first.java
+++ DSA/JAVA_LEARNING/functions_methods/Practice_Questions/first.java
@@ -1,18 +1,22 @@
 import java.util.*;
+// Write a Java method to compute the sum of the digits in an integer.
 public class first {
 
-    public static boolean isEven(int number){
-    if(number%2==0){
-        return true;
-    }else{
-        return false;
-    }      
+    public static int sumOfDigit (int number){
+        int sum=0,lastDigit=0;
+        while(number>0){
+            lastDigit=number%10;
+            sum=sum+lastDigit;
+            number=number/10;
+        }
+        return sum;
     }
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number: ");
-        int number=sc.nextInt();
-        System.out.println(isEven(number));
+        int number =sc.nextInt();
+        System.out.println(sumOfDigit(number));
         sc.close();
     }
     
