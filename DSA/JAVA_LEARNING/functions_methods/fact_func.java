43aeee77ddd4ece86745da862e04546d4ec01f01
khizer08 <syedkhizer2004@gmail.com>
2026-01-14 16:19:56 +0530

    readibility

--- DSA/JAVA_LEARNING/functions_methods/fact_func.java
+++ DSA/JAVA_LEARNING/functions_methods/fact_func.java
@@ -3,7 +3,7 @@ public class fact_func{
     public static int fact(int n){
         int fact=1;
         for(int i=1;i<=n;i++){
-            fact=fact*i;
+            fact=fact*i;// factorize main concept
         }
         return fact;
     }
