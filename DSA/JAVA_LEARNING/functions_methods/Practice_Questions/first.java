3b7293aa83bad2a2efd2dbc1777b5e8c85f284ed
khizer08 <syedkhizer2004@gmail.com>
2026-01-14 22:19:59 +0530

    isEven

--- DSA/JAVA_LEARNING/functions_methods/Practice_Questions/first.java
+++ DSA/JAVA_LEARNING/functions_methods/Practice_Questions/first.java
@@ -1,22 +1,19 @@
 import java.util.*;
 public class first {
 
-    public static void avg(){
-        Scanner sc=new Scanner(System.in);
-        int num1,num2,num3,avg;
-        System.out.print("Enter number 1: ");
-        num1=sc.nextInt();
-        System.out.print("Enter number 2: ");
-        num2=sc.nextInt();
-        System.out.print("Enter number 3: ");
-        num3=sc.nextInt();
-        avg=(num1+num2+num3)/3;
-        System.out.print("The average of "+num1+" "+num2+" "+num3+ " is: "+avg);
-        sc.close();
+    public static boolean isEven(int number){
+    if(number%2==0){
+        return true;
+    }else{
+        return false;
+    }      
     }
-
     public static void main(String[] args) {
-        avg();
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number=sc.nextInt();
+        System.out.println(isEven(number));
+        sc.close();
     }
     
 }
