195384e345fbf999f258166e8660e07aad6d98c8
khizer08 <syedkhizer2004@gmail.com>
2026-01-17 20:32:50 +0530

    inverted-half-pyramid

--- DSA/JAVA_LEARNING/Pattern_II/inverted_half_pyramid.java
+++ DSA/JAVA_LEARNING/Pattern_II/inverted_half_pyramid.java
@@ -2,14 +2,14 @@ import java.util.*;
 public class inverted_half_pyramid{
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int number,i,j;
-        System.out.print("Enter the value of number: ");
+        int number;
+        System.out.println("enter the number: ");
         number=sc.nextInt();
-        for(i=1;i<=number;i++){
-            for(j=1;j<=number-i;j++){
+        for(int i=1;i<=number;i++){
+            for(int j=1;j<=number-i;j++){
                 System.out.print(" ");
             }
-            for(j=1;j<=i;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print("*");
             }
             System.out.println();
