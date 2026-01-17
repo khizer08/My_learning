80b40e3a61ca5013ace69444cd3f073d101aea4f
khizer08 <syedkhizer2004@gmail.com>
2026-01-17 21:48:21 +0530

    z-pattern

--- DSA/JAVA_LEARNING/Pattern_II/z_pattern.java
+++ DSA/JAVA_LEARNING/Pattern_II/z_pattern.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class z_pattern {// try again
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter the number: ");
+        System.out.print("enter the value of n: ");
         int n = sc.nextInt();
         for(int i=1;i<=n;i++) {
             System.out.print("*");// first line logic
