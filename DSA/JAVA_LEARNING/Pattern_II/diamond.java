ca3b6aa8f6de864000f12ae368afe734e6d1e854
khizer08 <syedkhizer2004@gmail.com>
2026-01-18 14:48:26 +0530

    diamond-pattern

---
+++ DSA/JAVA_LEARNING/Pattern_II/diamond.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class diamond{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n=sc.nextInt();
+
+        //first half
+        //number of lines
+        for(int i=1;i<=n;i++){
+            //starting spaces
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //number of stars
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //second half
+        for(int i=n;i>=1;i--){
+
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
+
+// output:-
+//    *   
+//   ***
+//  *****
+// *******
+// *********
+// *******
+//  *****
+//   ***
+//    *
