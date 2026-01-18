bb562e93d987c249148f9c865973822814bc3db9
khizer08 <syedkhizer2004@gmail.com>
2026-01-18 13:53:20 +0530

    hollow_rhombus

--- DSA/JAVA_LEARNING/Pattern_II/hollow_rhombus.java
+++ DSA/JAVA_LEARNING/Pattern_II/hollow_rhombus.java
@@ -5,17 +5,21 @@ public class hollow_rhombus{
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number: ");
         int n=sc.nextInt();
+
+        // number of lines
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
+
+            for(int j=1;j<=n-i;j++){// starting spaces logic
                 System.out.print(" ");
             }
-            if(i==1 || i==n){
+
+            if(i==1 || i==n){// first and last line logic
                 for(int j=1;j<=n;j++){
                     System.out.print("*");
                 }
             }else{
                 System.out.print("*");
-                for(int j=1;j<=n-2;j++){
+                for(int j=1;j<=n-2;j++){// center spaces logic
                     System.out.print(" ");
                 }
                 System.out.print("*");
