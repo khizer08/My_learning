800d2fee33abb4533aab1d8ed1685463742ed8d6
khizer08 <syedkhizer2004@gmail.com>
2026-01-23 20:45:02 +0530

    array_reverse

---
+++ DSA/ARRAY/array_reverse.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class array_reverse{
+    public static int[] array_reverse_func(int number[]){ // as we are returning an array we need to use "int[]"" as the "return type".
+        int j=-1;
+        int number2[]=new int[number.length];
+        for(int i=number.length-1;i>=0;i--){
+            j++;
+            number2[j]=number[i];
+        }
+        return number2;
+    }
+    public static void main(String[] args) {
+        int number[]={12,34,56,78,33,11,56};
+        System.out.print(Arrays.toString(array_reverse_func(number)));// "Arrays.toString" is used because java doesnt know the content it will print the "references".
+    }
+}
