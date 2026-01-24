95d9519ec36dea82be416f28a44a256fb5653c54
khizer08 <syedkhizer2004@gmail.com>
2026-01-24 10:02:56 +0530

    array_pair

--- DSA/ARRAY/array_pair.java
+++ DSA/ARRAY/array_pair.java
@@ -1,7 +1,7 @@
 public class array_pair{
     public static void pair_func(int number[]){
         for(int i=0;i<number.length;i++){
-            int curr=number[i];
+            int curr=number[i]; //2,4,6,8,10
             for(int j=i+1;j<number.length;j++){
                 System.out.print("("+curr+","+number[j]+")");
             }
