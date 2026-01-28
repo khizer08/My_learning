17c0955bf22849944ec457ace619b7c82c4921b4
khizer08 <syedkhizer2004@gmail.com>
2026-01-28 11:23:50 +0530

    methods-conditions

--- Developement/frontend/react/basic-react-app/src/Intro.jsx
+++ Developement/frontend/react/basic-react-app/src/Intro.jsx
@@ -1,9 +1,15 @@
 import './Intro.css';
-function Intro({greet,name}){// "greet" and "name" are destructure props
+function Intro({greet,name,features}){// "greet" "name" "features" are destructure props.
   return (
     <>
-    <h1 className='greet'>{greet}</h1>
+    <h1>{greet}</h1>
     <h2 className='name'>I'm {name}</h2>
+    <h3>
+      {features.map((feature)=>(
+        <li>{feature}</li>
+        ))}
+    </h3>
+    {name=='khizer'?<p>authentic</p>:null}
     </>
   );
 }
--- Developement/frontend/react/basic-react-app/src/Intro_list.jsx
+++ Developement/frontend/react/basic-react-app/src/Intro_list.jsx
@@ -1,11 +1,10 @@
 import Intro from './Intro.jsx';
 
 function Intro_list(){
+    let features=["html"," css"," js"];
     return(
         <>
-        <Intro greet="hii"name="khizer"/>
-        <Intro greet="hello"name="syed"/>
-        <Intro greet="holla"name="huehue"/>
+        <Intro greet="hii"name="khizer" features={features} />
         </>
     );
 }
