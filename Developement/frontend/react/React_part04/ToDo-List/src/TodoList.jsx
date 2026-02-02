a72287d048abf4c735817046f7e2f9ed5a62fa1a
khizer08 <syedkhizer2004@gmail.com>
2026-02-02 12:00:54 +0530

    todo-functionality

--- Developement/frontend/react/React_part04/ToDo-List/src/TodoList.jsx
+++ Developement/frontend/react/React_part04/ToDo-List/src/TodoList.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 function TodoList() {
-  const [todos, setTodos] = useState([{task:"sample",id:uuidv4()}]); // as we are saving two informations we are using array of objects
+  const [todos, setTodos] = useState([
+    { task: "sample", id: uuidv4(), isDone: false },
+  ]); // as we are saving two informations as we are using array of objects
   const [newTodo, setNewTodo] = useState("");
 
   let addNewTask = () => {
-    setTodos((todos)=>{
-        return [...todos, {task:newTodo,id:uuidv4()}];
+    setTodos((todos) => {
+      return [...todos, { task: newTodo, id: uuidv4(), isDone: false }];
     });
     setNewTodo("");
   };
@@ -15,9 +17,54 @@ function TodoList() {
     setNewTodo(event.target.value);
   };
 
-  let deleteTodo=(id)=>{
-    setTodos((prevTodos)=>prevTodos.filter((todo)=>todo.id!=id)); // when ever this is triggered , a copy of entire todo list is made except for the element is was triggered.
-  }
+  let deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id != id)); // whenever this is triggered , a copy of entire todo list is made except for the element is was triggered.
+  };
+
+  let upperCaseAll = () => {
+    setTodos((todos) =>
+      todos.map((todo) => {
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      }),
+    );
+  };
+
+  let upperCaseOne = (id) => {
+    setTodos((todos) =>
+      todos.map((todo) => {
+        if (todo.id == id) {
+          return {
+            ...todo,
+            task: todo.task.toUpperCase(),
+          };
+        } else {
+          return todo;
+        }
+      }),
+    );
+  };
+
+  let isDoneOne = (id) => {
+    setTodos((todos) =>
+      todos.map((todo) => {
+        return todo.id == id ? { ...todo, isDone: !todo.isDone } : todo;
+      }),
+    );
+  };
+
+  let isDoneAll = () => {
+    setTodos((todos) =>
+      todos.map((todo) => {
+        return {
+          ...todo,
+          isDone: !todo.isDone,
+        };
+      }),
+    );
+  };
 
   return (
     <div>
@@ -38,11 +85,24 @@ function TodoList() {
       <ul>
         {todos.map((todo) => (
           <li key={todo.id}>
-            <span>{todo.task}</span>
-            <button onClick={()=>deleteTodo(todo.id)}>Delete</button> {/*Understand why are we using arrow function (revise notes)*/ }
-           </li>
+            <span
+              style={{ textDecoration: todo.isDone ? "line-through" : "none" }}
+            >
+              {todo.task}
+            </span>
+            &nbsp;&nbsp;&nbsp;
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            &nbsp;&nbsp;&nbsp;
+            {/*Understand why are we using arrow function (revise notes)*/}
+            <button onClick={() => upperCaseOne(todo.id)}>upperCaseOne</button>
+            &nbsp;&nbsp;&nbsp;
+            <button onClick={() => isDoneOne(todo.id)}>Done</button>
+          </li>
         ))}
       </ul>
+      <br></br>
+      <button onClick={upperCaseAll}>UpperCaseAll</button>&nbsp;&nbsp;&nbsp;
+      <button onClick={isDoneAll}>isDoneAll</button>
     </div>
   );
 }
