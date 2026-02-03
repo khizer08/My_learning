a0d57420b5531aca8da92f2b4c1b0bee16fe5c82
khizer08 <syedkhizer2004@gmail.com>
2026-02-03 12:17:25 +0530

    functions-as-props

--- Developement/frontend/react/React_part05/Lottery-game/src/App.jsx
+++ Developement/frontend/react/React_part05/Lottery-game/src/App.jsx
@@ -3,7 +3,7 @@ import Lottery from "./Lottery";
 import {sum} from "./helper.js";
 function App() {
 
-  let winCondition=(ticket)=>{
+  let winCondition=(ticket)=>{ // this function is used as "prop" in lottery.jsx(logic component)
     return sum(ticket)===15;
   }
   return (
