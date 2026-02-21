846ca6577a444f6addf013cf6c9c4558cd82a30f
khizer08 <syedkhizer2004@gmail.com>
2026-02-21 11:54:52 +0530

    revision

--- DSA/JAVA_LEARNING/Variable_And_DataTypes/StarPattern.java
+++ DSA/JAVA_LEARNING/Variable_And_DataTypes/StarPattern.java
@@ -1,17 +1,21 @@
-public class StarPattern{
-    public static void main(String args[]){
-        for(int i=4;i>=1;i--){
-            for(int j=1;j<=i;j++)
-            {
+import java.util.Scanner;
+
+public class StarPattern {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the value of n: ");
+        int n = sc.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
-
         }
     }
 }
 
-//output:-
+// output:-
 // ****
 // ***
 // **
