ce1b6dec210ef486d22eef4d9fb042d772888390
khizer08 <syedkhizer2004@gmail.com>
2026-02-22 21:49:19 +0530

    ternary-operator

--- DSA/JAVA_LEARNING/task.java
+++ DSA/JAVA_LEARNING/task.java
@@ -4,7 +4,7 @@ public class task {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int a = 10, b = 50, c = 30;
-        int max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        int max = (a > b) ? (a > c ? a : c) : (b > c ? b : c); // understand
         System.out.println("largest: " + max);
         sc.close();
     }
