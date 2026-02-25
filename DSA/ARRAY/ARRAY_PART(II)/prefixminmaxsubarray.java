a05557abc3055c56ed113c40c48c9487fd654151
khizer08 <syedkhizer2004@gmail.com>
2026-02-25 16:27:03 +0530

    core-logic

--- DSA/ARRAY/ARRAY_PART(II)/prefixminmaxsubarray.java
+++ DSA/ARRAY/ARRAY_PART(II)/prefixminmaxsubarray.java
@@ -16,7 +16,7 @@ public class prefixminmaxsubarray {
             for (int j = i; j < number.length; j++) {
                 int end = j;
 
-                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1]; // core logic.
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1]; // core logic-code-line.
 
                 if (maxSum < currSum) {
                     maxSum = currSum;
