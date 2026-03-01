ec9224c5e8c9bcb903ef1a03f6fe42d9b38a91e9
khizer08 <syedkhizer2004@gmail.com>
2026-03-01 14:24:12 +0530

    kadanes-algorithm

---
+++ DSA/ARRAY/ARRAY_PART(II)/kadanes.java
@@ -0,0 +1,24 @@
+public class kadanes {
+    public static void func(int number[]) {
+        int currSum = 0, maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < number.length; i++) {
+            currSum += number[i];
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            maxSum = Math.max(currSum, maxSum);
+        }
+        System.out.println("maximum Sum= " + maxSum);
+    }
+
+    public static void main(String[] args) {
+        int number[] = { 1, 2, 3, 4, 5 };
+        func(number);
+    }
+}
+
+// output:-
+// maximum Sum= 15
+
+// #NOTE:- For special case where all the elements of the array is negative run
+// a loop and find the min negative number and store it as "maxSum".
