a3c1b3a8ebc46571d8b94c6fbbb1f2b02af3b4d1
khizer08 <syedkhizer2004@gmail.com>
2026-03-06 12:32:38 +0530

    distinct-array-numbers

---
+++ DSA/ARRAY/ARRAY_PART(II)/PRACTICE/distinctarrayvalues.java
@@ -0,0 +1,18 @@
+public class distinctarrayvalues {
+    public static boolean func(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 4, 5 };
+        System.out.println(func(arr));
+    }
+}
