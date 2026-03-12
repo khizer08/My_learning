7e29b5aaa6df973fae52203580e1843a96b90e90
khizer08 <syedkhizer2004@gmail.com>
2026-03-12 19:38:45 +0530

    Selection-sort

--- DSA/ARRAY/BASIC_SORTING_ALGO/selectionsort.java
+++ DSA/ARRAY/BASIC_SORTING_ALGO/selectionsort.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
+
 public class selectionsort {
 
     public static void func(int nums[]) {
-        int n = nums.length, smallest = Integer.MAX_VALUE;
+        int n = nums.length;
         for (int i = 0; i < n - 1; i++) {
-            
-            for (int j = i + 1; j < n; j++) {
+            int minPos = i;
 
+            for (int j = i + 1; j < n; j++) {
+                if (nums[minPos] > nums[j]) {
+                    minPos = j;
+                }
             }
+
+            int temp = nums[minPos];
+            nums[minPos] = nums[i];
+            nums[i] = temp;
         }
 
     }
@@ -14,6 +23,8 @@ public class selectionsort {
     public static void main(String[] args) {
         int nums[] = { 1, 5, 2, 4, 3 };
         func(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
-// note :- pending
+// output:-
+// [1, 2, 3, 4, 5]
