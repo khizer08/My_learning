ca31fd08f1f290045731bb7d4fc718ba164eee1d
khizer08 <syedkhizer2004@gmail.com>
2026-03-12 05:50:35 +0530

    pending-selection-sort

---
+++ DSA/ARRAY/BASIC_SORTING_ALGO/selectionsort.java
@@ -0,0 +1,19 @@
+public class selectionsort {
+
+    public static void func(int nums[]) {
+        int n = nums.length, smallest = Integer.MAX_VALUE;
+        for (int i = 0; i < n - 1; i++) {
+            
+            for (int j = i + 1; j < n; j++) {
+
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 1, 5, 2, 4, 3 };
+        func(nums);
+    }
+}
+// note :- pending
