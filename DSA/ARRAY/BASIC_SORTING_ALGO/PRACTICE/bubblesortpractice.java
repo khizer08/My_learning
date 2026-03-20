c794cd6bbd80f0f3410e81c46eb3f45e5412e839
khizer08 <syedkhizer2004@gmail.com>
2026-03-20 10:44:07 +0530

    bubble-sort-descending

--- DSA/ARRAY/BASIC_SORTING_ALGO/PRACTICE/bubblesortpractice.java
+++ DSA/ARRAY/BASIC_SORTING_ALGO/PRACTICE/bubblesortpractice.java
@@ -1,22 +1,25 @@
+import java.util.Arrays;
+
 public class bubblesortpractice {
 
-    public static int func(int nums[]) {
+    public static void func(int nums[]) {
         int n = nums.length;
         for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n - (i + 1); j++) {
-                if (nums[i] > nums[j]) {
-                    int temp = nums[i];
-                    nums[i] = nums[i + 1];
-                    nums[i + 1] = temp;
+            for (int j = 0; j < n - (i + 1); j++) {
+
+                if (nums[j] < nums[j + 1]) {// descending sorting 
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
                 }
             }
         }
     }
-git add .
+
     public static void main(String[] args) {
         int nums[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
         func(nums);
-        System.out.println(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
 
