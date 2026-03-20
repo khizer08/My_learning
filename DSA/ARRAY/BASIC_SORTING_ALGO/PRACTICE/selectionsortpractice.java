93edc812e8b5a406f8d5b1d9d6d3283ad7fc0f04
khizer08 <syedkhizer2004@gmail.com>
2026-03-20 11:10:37 +0530

    selection-sort-descending

---
+++ DSA/ARRAY/BASIC_SORTING_ALGO/PRACTICE/selectionsortpractice.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class selectionsortpractice {
+
+    public static void func(int nums[]) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minpos = i;
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (nums[minpos] < nums[j]) { // descending sorting 
+                    minpos = j;
+                }
+            }
+
+            int temp = nums[minpos];
+            nums[minpos] = nums[i];
+            nums[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
+        func(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
