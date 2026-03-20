b1dc111ad36a629d2f0a3f9597835f8c8ee74cac
khizer08 <syedkhizer2004@gmail.com>
2026-03-20 12:09:45 +0530

    counting-sort-descending

---
+++ DSA/ARRAY/BASIC_SORTING_ALGO/PRACTICE/countingsortpractice.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class countingsortpractice {
+
+    public static void func(int nums[]) {
+        int n = nums.length;
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            largest = Math.max(largest, nums[i]);
+        }
+
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < n; i++) {
+            count[nums[i]]++;
+        }
+
+        int j = 0;
+
+        // descending sorting logic (this whole block).
+        for (int i = count.length - 1; i >= 0; i--) {
+            while (count[i] > 0) {
+                nums[j++] = i;
+                count[i]--;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 3, 6, 2, 1, 8, 7, 4, 5, 3, 1 };
+        func(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
