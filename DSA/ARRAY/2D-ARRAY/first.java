c1f234392d0acc2e3e4fe79719b5b7df311984b7
khizer08 <syedkhizer2004@gmail.com>
2026-03-29 10:42:27 +0530

    dsa-revise

--- DSA/ARRAY/2D-ARRAY/first.java
+++ DSA/ARRAY/2D-ARRAY/first.java
@@ -23,7 +23,7 @@ public class first {
             }
         }
 
-        // array elements print logic.
+        // array elements print logic .
         System.out.println(Arrays.deepToString(arr));
 
         // finding largest and smallest number logic and printing it .
