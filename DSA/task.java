7262338f4c58ee929926920d850f72cd881e51e3
khizer08 <syedkhizer2004@gmail.com>
2026-04-04 10:03:55 +0530

    arrays-O(log n) approach

--- DSA/task.java
+++ DSA/task.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class task{
     public static void main(String[] args) {
         Integer arr[]={2,5,1,4,3};
-        Arrays.sort(arr,Collections.reverseOrder());
+        Arrays.sort(arr,Collections.reverseOrder()); // built in functions
         System.out.println(Arrays.toString(arr));
     }
 }
