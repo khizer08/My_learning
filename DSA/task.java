767141bf67ff1accdd47fe5e784e267abe9e48df
khizer08 <syedkhizer2004@gmail.com>
2026-04-06 05:30:25 +0530

    1d-array-build-in-function-to-print

--- DSA/task.java
+++ DSA/task.java
@@ -5,6 +5,6 @@ public class task{
     public static void main(String[] args) {
         Integer arr[]={2,5,1,4,3};
         Arrays.sort(arr,Collections.reverseOrder()); // built in functions
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr)); // one d array printing
     }
 }
