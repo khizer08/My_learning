796cb79036b590ac8da294fe6f737072adf7c5f5
khizer08 <syedkhizer2004@gmail.com>
2026-04-30 12:57:45 +0530

    Prime-revision

--- AIML/python/Python_Fundamental(Part02)/Loops/multiplication_table.py
+++ AIML/python/Python_Fundamental(Part02)/Loops/multiplication_table.py
@@ -2,5 +2,5 @@ number=int(input("enter the number: "))
 
 i=1
 while(i<=10):
-    print(f"{number}X{i}={number*i}")
+    print(f"{number}X{i}={number*i}") # printing multiplication table of any number.
     i+=1
