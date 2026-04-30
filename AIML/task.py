ce432c26596da41fd456b2a25e6e25d6adac715d
khizer08 <syedkhizer2004@gmail.com>
2026-04-30 14:48:47 +0530

    string-indexing-slicing-revision

--- AIML/task.py
+++ AIML/task.py
@@ -1,6 +1,15 @@
-num=int(input("Enter the number: "))
-sum=0
-for i in range(1,num+1):
-    sum+=i
+# num=int(input("Enter the number: "))
+# sum=0
+# for i in range(1,num+1):
+#     sum+=i
 
-print(f"Sum of {num} natural numbers {sum}")
+# print(f"Sum of {num} natural numbers {sum}")
+
+
+# string indexing and slicing 
+
+# str="hello my name is khizer"
+
+# print(str[0]) # indexing
+
+# print(str[2:4]) # slicing
