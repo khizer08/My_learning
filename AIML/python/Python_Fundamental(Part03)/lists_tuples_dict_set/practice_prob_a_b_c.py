889ce1868f137f2cb960b212040132e7d54c04e5
khizer08 <syedkhizer2004@gmail.com>
2026-05-01 15:27:39 +0530

    fundamentals-practice

---
+++ AIML/python/Python_Fundamental(Part03)/lists_tuples_dict_set/practice_prob_a_b_c.py
@@ -0,0 +1,39 @@
+# DONE BY YOU :)
+info=[
+    ("syed","maths"),
+    ("khizer","science"),
+    ("owaise","maths"),
+    ("rahul","maths"),
+    ("syed","physics"),
+    ("khizer","chem"),
+    ("kumar","bio"),
+    ("leela","science")
+]
+
+
+# FIRST[A]
+# subjects=[]
+# for i in range(len(info)):
+#     subjects.append(info[i][1])
+# print(f"All the subjects are: \n{subjects}")
+
+# uni_Sub=set(subjects)
+# print(f"Unique subjects are: \n{uni_Sub}")
+
+
+# SECOND[B]
+# for name,sub in info:
+#     if sub=="maths":
+#         print(name)
+
+
+# THIRD[C]
+# dict1={}
+# for name,sub in info:
+#     if(dict1.get(name)==None):
+#         dict1.update({name:set()})
+#         dict1[name].add(sub)
+#     else:
+#         dict1[name].add(sub)
+
+# print(dict1)
