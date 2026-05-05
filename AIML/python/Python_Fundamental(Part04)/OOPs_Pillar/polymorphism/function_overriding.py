86284d40be2f7a131c23f8f11a2b987d626fdb56
khizer08 <syedkhizer2004@gmail.com>
2026-05-06 04:29:43 +0530

    func-override

---
+++ AIML/python/Python_Fundamental(Part04)/OOPs_Pillar/polymorphism/function_overriding.py
@@ -0,0 +1,10 @@
+class Employee:
+    def get_designation(self):
+        print("The designation is: Employee")
+        
+class Teacher(Employee):
+    def get_designation(self): # "same method name → this overrides the parent class method"
+        print("The designation is: Teacher")
+        
+t1=Teacher()
+t1.get_designation()
