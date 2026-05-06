24d5afcc6679c9b83ec7bd0b52ccf6d8db354536
khizer08 <syedkhizer2004@gmail.com>
2026-05-07 04:28:39 +0530

    Duck-Typing

---
+++ AIML/python/Python_Fundamental(Part04)/OOPs_Pillar/polymorphism/duck_typing.py
@@ -0,0 +1,14 @@
+class Teacher():
+    def get_designation(self):
+        print("The designation is: Teacher")
+        
+class Accountant():
+    def get_designation(self):
+        print("The designation is: Accountant")
+        
+        
+t1=Teacher()
+t1.get_designation()
+
+a1=Accountant()
+a1.get_designation()
