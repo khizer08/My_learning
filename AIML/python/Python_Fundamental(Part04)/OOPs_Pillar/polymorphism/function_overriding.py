f9ffd4703ff44cf76b3b6589674321a6bcc8431d
khizer08 <syedkhizer2004@gmail.com>
2026-05-08 04:52:21 +0530

    python-partial-oops

--- AIML/python/Python_Fundamental(Part04)/OOPs_Pillar/polymorphism/function_overriding.py
+++ AIML/python/Python_Fundamental(Part04)/OOPs_Pillar/polymorphism/function_overriding.py
@@ -3,7 +3,7 @@ class Employee:
         print("The designation is: Employee")
         
 class Teacher(Employee):
-    def get_designation(self): # "same method name → this overrides the parent class method"
+    def get_designation(self): # "same method name , this overrides the parent class method"
         print("The designation is: Teacher")
         
 t1=Teacher()
