158f1c826524d67e7e9934465d526a6d2688c8bc
khizer08 <syedkhizer2004@gmail.com>
2026-07-03 22:14:44 +0530

    dsa

--- DSA/task.java
+++ DSA/task.java
@@ -1,10 +1,6 @@
-import java.util.Collections;
-import java.util.Arrays;
-
+import java.util.*;
 public class task{
-    public static void main(String[] args) {
-        Integer arr[]={2,5,1,4,3};
-        Arrays.sort(arr,Collections.reverseOrder()); // built in functions
-        System.out.println(Arrays.toString(arr)); // one d array printing
+    public static void main(String [] args){
+        System.out.println("hello");
     }
 }
