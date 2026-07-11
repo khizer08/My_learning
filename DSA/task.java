cb44b9c0ed1a71667b290f55150e6190cc2e2eab
khizer08 <syedkhizer2004@gmail.com>
2026-07-12 00:12:16 +0530

    Dsa-revision

--- DSA/task.java
+++ DSA/task.java
@@ -1,7 +1,13 @@
-import java.util.*;
-public class task{
-    public static void main(String [] args){
-        int a=2%1;
-        System.out.println(a);
+public class task {
+    public static void main(String arg[]) {
+        int number = 10988;
+        int last_digit;
+        int reverse_number=0;
+        while (number>0) {
+            last_digit = number % 10;
+            reverse_number=last_digit+reverse_number*10;
+            number=number/10;
+        }
+        System.out.println(reverse_number);
     }
 }
