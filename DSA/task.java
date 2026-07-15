d93593c356e55c18265987c724b1c778c7e17a13
khizer08 <syedkhizer2004@gmail.com>
2026-07-15 15:35:53 +0530

    Array-revision

--- DSA/task.java
+++ DSA/task.java
@@ -1,28 +1,27 @@
-// import java.util.*;
+import java.util.*;
 
 public class task {
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
-        }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
 
-    }
-
-    public static void count_prime(int num) {
-        for (int i = 1; i <= num; i++) {
-            if (isPrime(i) == true) {
-                System.out.println(i);
-            }
+    public static void reverse_arr(int arr[]) {
+        int size = arr.length;
+        int last = size - 1;
+        int first = 0;
+        while (first < last) {
+            int temp = arr[last];
+            arr[last] = arr[first];
+            arr[first] = temp;
+            last--;
+            first++;
         }
     }
 
     public static void main(String arg[]) {
-        count_prime(10);
+        int arr[] = { 2, 4, 6, 8, 10 };
+
+        reverse_arr(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
     }
 }
