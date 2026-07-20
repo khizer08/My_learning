680ed5797b15d22de41be1c69adc5e909ae6c794
khizer08 <syedkhizer2004@gmail.com>
2026-07-20 13:09:08 +0530

    Amazon-classical-problem

--- DSA/STRINGS/task.java
+++ DSA/STRINGS/task.java
@@ -1,15 +1,33 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class task {
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        // String str;
-        // System.out.print("Enter your name: ");
-        // str=sc.nextLine();
-        // System.out.println(str);
+    public static int compress(char chars[]) {
+        StringBuilder sb = new StringBuilder("");
+        for (int i = 0; i < chars.length; i++) {
+            Integer count = 1;
+            while (i < chars.length - 1 && chars[i] == chars[i + 1]) {
+                count++;
+                i++;
+            }
+            sb.append(chars[i]);
+            if (count > 1) {
+                sb.append(count.toString());
+            }
 
-        String firstName=sc.next();
-        String lastName=sc.next();
-        System.out.println(firstName+" "+lastName);
+        }
+        String output = sb.toString();
+        char[] arr = output.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            chars[i] = arr[i];
+        }
+
+        return arr.length;
+    }
+
+    public static void main(String[] args) {
+        char chars[] = { 'a', 'a', 'b', 'b', 'c' };
+        System.out.println(compress(chars));
     }
 }
