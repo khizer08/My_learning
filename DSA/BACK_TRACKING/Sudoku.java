a5874f64976d7c3263f2409a1cb276082ba41471
khizer08 <syedkhizer2004@gmail.com>
2026-09-10 15:14:21 +0530

    Sudoku-finder

--- DSA/BACK_TRACKING/Sudoku.java
+++ DSA/BACK_TRACKING/Sudoku.java
@@ -1,6 +1,73 @@
 public class Sudoku {
 
-    public static
+    public static boolean isSafe(int sudoku[][], int row, int col, int digit) {
+        // column
+        for (int i = 0; i <= 8; i++) {
+            if (sudoku[i][col] == digit) {
+                return false;
+            }
+        }
+
+        // row
+        for (int j = 0; j <= 8; j++) {
+            if (sudoku[row][j] == digit) {
+                return false;
+            }
+        }
+
+        // grid
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (sudoku[i][j] == digit) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean sudokuSolver(int sudoku[][], int row, int col) {
+        // base case
+        if (row == 9) {
+            return true;
+        }
+        // recursion
+
+        int nextRow = row, nextCol = col + 1;
+        if (col + 1 == 9) {
+            nextRow = row + 1;
+            nextCol = 0;
+        }
+
+        if (sudoku[row][col] != 0) {
+            return sudokuSolver(sudoku, nextRow, nextCol);
+        }
+
+        for (int digit = 1; digit <= 9; digit++) {
+            if (isSafe(sudoku, row, col, digit)) {
+                sudoku[row][col] = digit;
+                if (sudokuSolver(sudoku, nextRow, nextCol)) {
+                    return true;
+                }
+
+                sudoku[row][col] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    public static void printSodoku(int sudoku[][]) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(sudoku[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String args[]) {
         int sudoku[][] = {
@@ -14,5 +81,12 @@ public class Sudoku {
                 { 0, 4, 9, 0, 3, 0, 0, 5, 7 },
                 { 8, 2, 7, 0, 0, 9, 0, 1, 3 }
         };
+
+        if (sudokuSolver(sudoku, 0, 0)) {
+            System.out.println("Solution Exist");
+            printSodoku(sudoku);
+        } else {
+            System.out.println("Solution not Exist!!");
+        }
     }
 }
